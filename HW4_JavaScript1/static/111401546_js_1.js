fcb83e2fd2365d28cd017c7ef88c98b1fb439dd3
kurochan1901 <superstickman19.mc@gmail.com>
2025-09-25 14:11:27 +0800

    alert not number

--- HW4_JavaScript1/static/111401546_js_1.js
+++ HW4_JavaScript1/static/111401546_js_1.js
@@ -1,1 +1,14 @@
-alert("Hello, world!");
+let answer = Math.floor(Math.random() * 101);
+let count = 0;
+console.log(answer);
+
+function guessNumber() {
+    const guessInput = parseInt(document.getElementById("guessInput").value);
+    count++;
+
+    if (isNaN(guessInput)) {
+        alert("請輸入一個有效的數字！");
+        return;
+    };
+
+};
