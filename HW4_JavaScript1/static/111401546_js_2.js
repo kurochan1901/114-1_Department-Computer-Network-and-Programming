8c98d4e64244d287ab302bcae4900e1390ab8d99
kurochan1901 <superstickman19.mc@gmail.com>
2025-09-25 15:28:21 +0800

    all button

--- HW4_JavaScript1/static/111401546_js_2.js
+++ HW4_JavaScript1/static/111401546_js_2.js
@@ -3,3 +3,12 @@ for (let i=9; i>=0; i--) {
     document.write(`<button onclick="appendNumber('${i}')">${i}</button>`); // 數字按鈕
     if (i===7||i===4||i===1) document.write('<br>');
 }
+document.write('<button onclick="clearDisplay()">Clear</button>');
+document.write('<br><br>');
+
+const operators = ['+', '-', '*', '/', '(', ')'];
+for (let op of operators) {
+  document.write(`<button onclick="append('${op}')">${op}</button>`);
+};
+
+document.write('<br><button onclick="calculate()">Enter</button>');
