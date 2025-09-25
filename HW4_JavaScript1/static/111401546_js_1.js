5217fea24cb3c53b13fcb5e54f20e0e9d77fcec0
kurochan1901 <superstickman19.mc@gmail.com>
2025-09-25 14:32:07 +0800

    show guess count, and final test fin.

--- HW4_JavaScript1/static/111401546_js_1.js
+++ HW4_JavaScript1/static/111401546_js_1.js
@@ -1,4 +1,6 @@
 let answer = Math.floor(Math.random() * 101);
+let min = 0;
+let max = 100;
 let count = 0;
 console.log(answer);
 
@@ -14,6 +16,19 @@ function guessNumber() {
     if (guessInput < 0 || guessInput > 100) {
         alert("請輸入0到100之間的數字！");
         return;
+    };
+    if (guessInput > answer) {
+        max = guessInput;
+        alert(`太大了，介於${min} ~ ${max}之間`);
+    }else if (guessInput < answer) {
+        min = guessInput;
+        alert(`太小了，介於${min} ~ ${max}之間`);
+    }else {
+        alert(`恭喜你答對了！答案是${answer}。你總共猜了${count}次。`);
+        answer = Math.floor(Math.random() * 101);
+        min = 0;
+        max = 100;
+        count = 0;
     };
 
 };
