dba5712becaef4ca559a315b6e7a92cfa624ba1c
kurochan1901 <superstickman19.mc@gmail.com>
2025-09-25 15:41:40 +0800

    button function

--- HW4_JavaScript1/static/111401546_js_2.js
+++ HW4_JavaScript1/static/111401546_js_2.js
@@ -17,7 +17,7 @@ document.write(`
 
 document.write('<input type="text" id="display" readonly><br><br>'); // 顯示輸入和結果的欄位
 for (let i=9; i>=0; i--) {
-    document.write(`<button onclick="appendNumber('${i}')">${i}</button>`); // 數字按鈕
+    document.write(`<button onclick="append('${i}')">${i}</button>`); // 數字按鈕
     if (i===7||i===4||i===1) document.write('<br>');
 }
 document.write('<button onclick="clearDisplay()" style="width:106px">Clear</button>');
@@ -29,3 +29,21 @@ for (let op of operators) {
 };
 
 document.write('<br><button onclick="calculate()" style="width:106px">Enter</button>');
+
+//按鈕 to 輸入框
+function append(value) {
+    document.getElementById('display').value += value;
+};
+
+//計算
+function calculate() {
+    const expression = document.getElementById('display').value;
+    const result = eval(expression);
+    alert(`算式：${expression}\n答案：${result}`);
+    document.getElementById('display').value = result;
+};
+
+//清除
+function clearDisplay() {
+    document.getElementById('display').value = '';
+};
