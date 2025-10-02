b8dd32650260add5c10efeaf5578de6f075aa6d1
kurochan1901 <superstickman19.mc@gmail.com>
2025-10-02 20:03:07 +0800

    數量加減按鈕

--- HW5_JavaScript2/static/111401546_js_2.js
+++ HW5_JavaScript2/static/111401546_js_2.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
         itemCheckboxes.forEach(cb => cb.checked = selectAll.checked);
         updateTotal();
     });
-
+    //全選單選連動
     itemCheckboxes.forEach(cb => {
         cb.addEventListener("change", () => {
         const allChecked = [...itemCheckboxes].every(cb => cb.checked);
@@ -16,4 +16,26 @@ document.addEventListener("DOMContentLoaded", () => {
         updateTotal();
         });
     });
+});
+
+//數量加減
+document.querySelectorAll("tr").forEach(row => {
+    const minusBtn = row.querySelector(".minus");
+    const plusBtn = row.querySelector(".plus");
+    const quantityInput = row.querySelector(".quantity");
+
+    minusBtn.addEventListener("click", () => {
+        let quantity = parseInt(quantityInput.textContent);
+        if (quantity > 1) quantity--;
+        quantityInput.textContent = quantity;
+        updateTotal();
+    });
+
+    plusBtn.addEventListener("click", () => {
+        let quantity = parseInt(quantityInput.textContent);
+        quantity++;
+        quantityInput.textContent = quantity;
+        updateTotal();
+    });
+
 });
