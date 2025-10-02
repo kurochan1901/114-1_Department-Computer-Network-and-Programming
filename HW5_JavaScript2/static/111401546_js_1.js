aa728d286c6f4ae6348029299a05382e5d706655
kurochan1901 <superstickman19.mc@gmail.com>
2025-10-02 14:06:45 +0800

    更新畫面上提示

---
+++ HW5_JavaScript2/static/111401546_js_1.js
@@ -0,0 +1,35 @@
+let answer = Math.floor(Math.random() * 101);
+let min = 0;
+let max = 100;
+let count = 0;
+console.log(answer);
+
+function guessNumber() {
+    const guessInput = parseInt(document.getElementById("guessInput").value);
+    const hint = document.getElementById("hint");
+    count++;
+
+    if (isNaN(guessInput)){
+        hint.textContent = "請輸入一個有效的數字！";
+        return;
+    };
+
+    if (guessInput < 0 || guessInput > 100) {
+        hint.textContent = "請輸入0到100之間的數字！";
+        return;
+    };
+    if (guessInput > answer) {
+        max = guessInput;
+        hint.textContent = `太大了，介於${min} ~ ${max}之間`;
+    }else if (guessInput < answer) {
+        min = guessInput;
+        hint.textContent = `太小了，介於${min} ~ ${max}之間`;
+    }else {
+        alert(`恭喜你答對了！答案是${answer}。\n你總共猜了${count}次。`);
+        answer = Math.floor(Math.random() * 101);
+        min = 0;
+        max = 100;
+        count = 0;
+    };
+
+};
