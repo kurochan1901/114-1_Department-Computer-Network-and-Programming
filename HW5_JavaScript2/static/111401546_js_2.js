8ffdada1e77b61c2e1215a233d2a7322ed7cf213
kurochan1901 <superstickman19.mc@gmail.com>
2025-10-02 19:40:20 +0800

    修好核取方塊 終於

--- HW5_JavaScript2/static/111401546_js_2.js
+++ HW5_JavaScript2/static/111401546_js_2.js
@@ -1,16 +1,19 @@
-//選取方塊
-const selectAll = document.getElementById("selectAll");
-const itemCheckboxes = document.querySelectorAll(".itemCheckbox");
+document.addEventListener("DOMContentLoaded", () => {
+    const selectAll = document.getElementById("selectAll");
+    const itemCheckboxes = document.querySelectorAll(".itemCheckbox");
 
-selectAll.addEventListener("change", () => {
-    itemCheckboxes.forEach(cb => cb.checked = selectAll.checked);
-    updateTotal();
-});
 
-itemCheckboxes.forEach(cb => {
-    cb.addEventListener("change", () => {
-    const allChecked = [...itemCheckboxes].every(cb => cb.checked);
-    selectAll.checked = allChecked;
-    updateTotal();
+    //全選
+    selectAll.addEventListener("change", () => {
+        itemCheckboxes.forEach(cb => cb.checked = selectAll.checked);
+        updateTotal();
+    });
+
+    itemCheckboxes.forEach(cb => {
+        cb.addEventListener("change", () => {
+        const allChecked = [...itemCheckboxes].every(cb => cb.checked);
+        selectAll.checked = allChecked;
+        updateTotal();
+        });
     });
 });
